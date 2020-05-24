88c688ee9cc437d37120f26fbe32c76980c77224
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2020-05-24 19:40:41 +0200

    Added more tests for BSPPak

--- BSPUtilsTest/BSPPak/AppTest.cs
+++ BSPUtilsTest/BSPPak/AppTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using BSPUtilsTest.TestUtil;
+using LibBSP;
 using Xunit;
 using App = BSPPak;
 using Program = BSPPak.Program;
@@ -42,6 +44,52 @@ namespace BSPUtilsTest.BSPPak
                 ContentPath = "testdataInvalid"
             };
             Assert.Throws<App.InvalidOptionException>(() => Program.ValidateOptions(opts));
+        }
+
+        [Fact]
+        public void TestPackFiles()
+        {
+            using var temp = new TempFolder();
+            var bspPath = $"{temp.FolderPath}/map.bsp";
+            File.Copy("testdata/map.bsp", bspPath);
+
+            var filesToPack = new[]
+            {
+                "testdata/map.vmf"
+            };
+
+            // Pack the files in the filesToPackList
+            Program.PackFiles(new Options
+            {
+                BSPPath = bspPath,
+                ContentPath = "testdata"
+            }, filesToPack);
+
+            // Load the BSP to make sure we can find the files
+
+            var bsp = new BSP(bspPath);
+
+            var pakLump = (PakfileLump)bsp.Lumps[(int)LumpType.Pakfile];
+            var archive = pakLump.OpenArchiveStream(ZipArchiveMode.Read);
+
+            // Intersect the names of the paklump's entries with our set of files. The resulting list should be of equal length as our filesToPack if all of them exist in the lump.
+            Assert.Equal(filesToPack.Length, 
+                archive.Entries
+                    .Select(entry => entry.FullName)
+                    .Intersect(filesToPack.Select(str => Program.ToRelativePath(str, "testdata"))).Count());
+
+            pakLump.CloseArchiveStream();
+        }
+
+        [Fact]
+        public void TestRelativePath()
+        {
+            Assert.Equal(@"Path.txt", Program.ToRelativePath(@"C:\Test\Path.txt", @"C:\Test"));
+            Assert.Equal(@"Path.txt", Program.ToRelativePath(@"C:\Test\Path.txt", @"C:\Test\"));
+            Assert.Equal(@"Path.txt", Program.ToRelativePath(@"C:/Test/Path.txt", @"C:/Test"));
+            Assert.Equal(@"Path.txt", Program.ToRelativePath(@"C:/Test/Path.txt", @"C:/Test/"));
+
+            Assert.Throws<ArgumentException>(() => { Program.ToRelativePath(@"C:\Test\Path.txt", @"C:\Hello"); });
         }
 
     }
---
+++ BSPUtilsTest/BSPPak/FileFinderTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using BSPPak;
+using App = BSPLumpExtract;
+using Xunit;
+
+namespace BSPUtilsTest.BSPPak
+{
+    public class FileFinderTest
+    {
+        [Fact]
+        public void TestFileFinder()
+        {
+            var files = FileFinder.Find("testdata");
+
+            Assert.Equal(1, files.Count);
+            Assert.EndsWith("map.vmf", files.First());
+        }
+    }
+}
